02d78a0bbf90d8322972a3285407ef9ad85e6d22
ashiqurras <ashiqurrussel2@gmail.com>
2022-03-05 04:08:22 +0100

    Update Functionality Added

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -15,6 +15,9 @@ export class EmployeeDashboardComponent implements OnInit {
   employeeModelObj: EmployeeModel = new EmployeeModel();
   //Property
   emplyoeeData!: any;
+  showAddButton!: boolean;
+  showUpdateButton!: boolean
+
 
   //Constructor for dependency injection
   constructor(private formbuilder: FormBuilder, private api: ApiService) { }
@@ -74,6 +77,44 @@ export class EmployeeDashboardComponent implements OnInit {
       this.getAllEmployee(); // after delete button press page will refresh and load data
     })
 
+  }
+
+  // Click method for  appearing prevoius Employee value on form input
+
+  onEdit(row: any) {
+    this.showAddButton = false;
+    this.showUpdateButton = true
+    this.employeeModelObj.id = row.id;  // store id when click 
+    this.formValue.controls['firstName'].setValue(row.firstName);
+    this.formValue.controls['lastName'].setValue(row.lastName);
+    this.formValue.controls['email'].setValue(row.email);
+    this.formValue.controls['phone'].setValue(row.phone);
+    this.formValue.controls['salary'].setValue(row.salary);
+  }
+  //Method for updating employee data
+  updateEmployeeDetails() {
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.email = this.formValue.value.email;
+    this.employeeModelObj.phone = this.formValue.value.phone;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api.updateEmployee(this.employeeModelObj, this.employeeModelObj.id)
+      .subscribe(res => {
+        alert("Updated Succedfully")
+
+        let ref = document.getElementById('cancel');
+        ref?.click();
+        this.formValue.reset();
+        this.getAllEmployee();
+      })
+  }
+
+  // Method for resetting Form value when << Add Employee >> button click
+  clickAddEmployee() {
+    this.formValue.reset();
+    this.showAddButton = true;
+    this.showUpdateButton = false
   }
 
 }
